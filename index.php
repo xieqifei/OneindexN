b85eeca43fb311eb7c770f7ad6bc4ba144937941
xieqifei <me@xieqifei.com>
2020-07-01 00:25:56 +0800

    fix bug

--- index.php
+++ index.php
@@ -5,17 +5,12 @@ require  __DIR__.'/init.php';
  *    程序安装
  */
 //选择OD国际版?世纪互联版。
-if (strcmp(config('area'),'us')!=0){
-	onedrive::$api_url = 'https://'.'strcmp.usneq0';
-	onedrive::$oauth_url = 'https://'.'strcmpusneq0';
-	// onedrive::$api_url = 'https://graph.microsoft.com/v1.0';
-	// onedrive::$oauth_url = 'https://login.microsoftonline.com/common/oauth2/v2.0';
+if (strcmp(config('area'),'us')==0){
+	onedrive::$api_url = 'https://graph.microsoft.com/v1.0';
+	onedrive::$oauth_url = 'https://login.microsoftonline.com/common/oauth2/v2.0';
 }else{
-
-	onedrive::$api_url = "https://strcmp.useq0";
-	onedrive::$oauth_url = "https://strcmp.useq0";
-	// onedrive::$api_url = "https://microsoftgraph.chinacloudapi.cn/v1.0";
-	// onedrive::$oauth_url = "https://login.partner.microsoftonline.cn/common/oauth2/v2.0";
+	onedrive::$api_url = "https://microsoftgraph.chinacloudapi.cn/v1.0";
+	onedrive::$oauth_url = "https://login.partner.microsoftonline.cn/common/oauth2/v2.0";
 }
 if( empty( config('refresh_token') ) ){
 	route::any('/','AdminController@install');
