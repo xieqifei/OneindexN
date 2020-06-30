a48a02906b61970a709bd8f17019a1439a711051
xieqifei <me@xieqifei.com>
2020-06-30 23:51:50 +0800

    fix bug

--- index.php
+++ index.php
@@ -3,20 +3,22 @@
 require  __DIR__.'/init.php';
 onedrive::$api_url = "https://microsoftgraph.chinacloudapi.cn/v1.0";
 onedrive::$oauth_url = "https://login.partner.microsoftonline.cn/common/oauth2/v2.0";
+//onedrive::$api_url = "https://microsoftgraph.chinacloudapi.cn/v1.0";
+//onedrive::$oauth_url = "https://login.partner.microsoftonline.cn/common/oauth2/v2.0";
 /**
  *    程序安装
  */
 //选择OD国际版?世纪互联版。
-if (strcmp(config('area'),'us')==0){
-	onedrive::$api_url = 'https://graph.microsoft.com/v1.0';
-	onedrive::$oauth_url = 'https://login.microsoftonline.com/common/oauth2/v2.0';
-}else{
-	onedrive::$api_url = "https://microsoftgraph.chinacloudapi.cn/v1.0";
-	onedrive::$oauth_url = "https://login.partner.microsoftonline.cn/common/oauth2/v2.0";
-}
-if( empty( config('refresh_token') ) ){
-	route::any('/','AdminController@install');
-}
+// if (strcmp(config('area'),'us')==0){
+// 	onedrive::$api_url = 'https://graph.microsoft.com/v1.0';
+// 	onedrive::$oauth_url = 'https://login.microsoftonline.com/common/oauth2/v2.0';
+// }else{
+// 	onedrive::$api_url = "https://microsoftgraph.chinacloudapi.cn/v1.0";
+// 	onedrive::$oauth_url = "https://login.partner.microsoftonline.cn/common/oauth2/v2.0";
+// }
+// if( empty( config('refresh_token') ) ){
+// 	route::any('/','AdminController@install');
+// }
 
 /**
  *    系统后台
