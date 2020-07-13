7ab09ca82bd4c7d6af9669901dfcb3ae30b8e0f6
xieqifei <me@xieqifei.com>
2020-07-13 11:46:43 +0800

    search function

--- view/themes/nexmoe/list.php
+++ view/themes/nexmoe/list.php
@@ -16,6 +16,8 @@ function file_ico($item){
 ?>
 
 <?php view::begin('content');?>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js" integrity="sha384-qlmct0AOBiA2VPZkMY3+2WqkHtIQ9lSdAsAn5RUJD/3vA5MKDgSGcdmIv4ycVxyn" crossorigin="anonymous"></script>
 
 <?php if(is_login()):?>
 <div class="mdui-container-fluid" >
