fbd84cf79330da305b1a903bf15ef4b01a8a91de
xieqifei <me@xieqifei.com>
2020-07-15 02:13:43 +0800

    优化全局搜索

--- view/themes/nexmoe/list.php
+++ view/themes/nexmoe/list.php
@@ -16,7 +16,9 @@ function file_ico($item){
 ?>
 
 <?php view::begin('content');?>
-
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="https://cdn.bootcdn.net/ajax/libs/clipboard.js/2.0.6/clipboard.min.js"></script>
+<script src="statics\themes\nexmoe\js\nexmoe.js"></script>
 <?php if(is_login()):?>
 <div class="mdui-container-fluid" >
 	<div class="nexmoe-item">
@@ -225,8 +227,6 @@ function file_ico($item){
   </div>
 </div>
 <!-- <script src="https://cdn.jsdelivr.net/gh/xieqifei/OneindexN@v1.31/statics/js/nexmoe.js"></script> -->
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-<script src="https://cdn.bootcdn.net/ajax/libs/clipboard.js/2.0.6/clipboard.min.js"></script>
-<script src="statics\themes\nexmoe\js\nexmoe.js"></script>
+
 
 <?php view::end('content');?>
